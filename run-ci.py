8dfb0a0d23ea19a930de3b69a4b549b517655d7b
Théo Delrieu <theo.delrieu@tanker.io>
2021-08-25 10:31:49 +0200

    add build-setup package

--- run-ci.py
+++ run-ci.py
@@ -318,6 +318,8 @@ def build_and_test(
         builder.prepare(update, tanker_ref)
         if test:
             builder.test()
+    # TODO how to test it?
+    tankerci.run("cargo", "build", cwd=Path.cwd() / "build-setup")
 
 
 def deploy(args: argparse.Namespace) -> None:
@@ -334,6 +336,7 @@ def deploy(args: argparse.Namespace) -> None:
     tankerci.bump_files(version)
 
     tankerci.run("cargo", "publish", "--allow-dirty", f"--registry={registry}", cwd="sdk")
+    tankerci.run("cargo", "publish", f"--registry={registry}", cwd="build-setup")
 
 
 def main() -> None:
