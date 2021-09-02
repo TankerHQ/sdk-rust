1c1c93af4326cc6be2d15c9f821e99365457ae10
jerem <jeremy.tellaa@tanker.io>
2021-09-02 16:10:07 +0200

    Revert "add build-setup package"
    This reverts commit 67d6736b12fcc04bb43c853fc63b73042b6dc7c7.

--- run-ci.py
+++ run-ci.py
@@ -318,7 +318,6 @@ def build_and_test(
         builder.prepare(update, tanker_ref)
         if test:
             builder.test()
-    tankerci.run("cargo", "build", cwd=Path.cwd() / "build-setup")
 
 
 def deploy(args: argparse.Namespace) -> None:
@@ -335,7 +334,6 @@ def deploy(args: argparse.Namespace) -> None:
     tankerci.bump_files(version)
 
     tankerci.run("cargo", "publish", "--allow-dirty", f"--registry={registry}", cwd="sdk")
-    tankerci.run("cargo", "publish", f"--registry={registry}", cwd="build-setup")
 
 
 def main() -> None:
